5a56076275816e4ee0d754cabed301ca2a404a2b
muhammad-munij-ali <komaster9009@gmail.com>
2024-04-12 09:34:11 -0400

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -36,7 +36,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
       <Route path='' element={<Home/>} />
-      <Route path='about' element={<About/>} />
+      <Route path='about' element={<About/>} >
+        <Route path='hitesh' />
+      </Route>
       <Route path='contact' element={<Contact/>} />
       <Route path='user/:userid' element={<User/>} />
       <Route 
